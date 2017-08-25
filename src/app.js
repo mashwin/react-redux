0af9343c07b66e143210ca12a2984fc8b2478534
mashwin <ashwinmorey@gmail.com>
2017-08-25 13:46:45 +0530

    simple redux application

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log('testing application js');
+import { createStore } from 'redux';
+
+// STEP 3: Define Reducer
+const reducer = function(state = {}, action) {
+  switch (action.type) {
+    case 'ADD_USER':
+      return state = action.payload;
+      break;
+    default:
+  }
+
+  return state;
+}
+
+// STEP 1: Create Store
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+
+// STEP 2: Dispatch Actions
+store.dispatch({
+  type: 'ADD_USER',
+  payload: {
+    'username': 'mashwin',
+    'email': 'mashwin@gmail.com'
+  }
+});
